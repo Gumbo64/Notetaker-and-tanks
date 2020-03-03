c892fcdb89f0a219bebfde2577ed949be4a7d857
Gumbo64 <35547211+Gumbo64@users.noreply.github.com>
2020-03-03 18:31:23 +1100

    Initial commit

---
+++ First.js
@@ -0,0 +1,18 @@
+console.log('hello world');
+number = 1
+let basicallydictionary = {
+    a: 'a',
+    b: 'b'
+};
+basicallydictionary.a = basicallydictionary['a']
+// same thing //
+console.log(basicallydictionary.a);
+
+function numberplus() {
+    number = number + 1;
+    return(number);
+}
+
+console.log(useless(basicallydictionary));
+
+
